4a6fa02d1e586c5f7b0f63600833fe7b975955ee
Dimas Begunoff <dbegunoff@gmail.com>
2014-11-23 09:19:26 -0800

    tracking return values

--- src/Spy.php
+++ src/Spy.php
@@ -6,13 +6,16 @@ class Spy {
 
 	private $intel = array();
 
+	private $data = array();
+
 	private $target = null;
 
 	public function __construct() {
 		call_user_func_array( array( $this, 'on' ), func_get_args() );
 	}
 
-	public function report() {
+	// wip
+	private function report() {
 		return $this->intel;
 	}
 
@@ -22,18 +25,50 @@ class Spy {
 			\Patchwork\replace( $func, function() {
 				$func = \Patchwork\Stack\top( 'function' );
 				if ( ! isset( $this->intel[ $func ] ) ) {
-					$this->intel[ $func ] = [];
+					$this->intel[ $func ] = array();
 				}
-				array_push( $this->intel[ $func ], func_get_args() );
-				\Patchwork\pass();
+				$ret = \Patchwork\callOriginal();
+				array_push( $this->intel[ $func ], array(
+					'arg' => func_get_args(),
+					'ret' => $ret,
+				) );
+				array_push( $this->data, array(
+					'func' => $func,
+					'arg' => func_get_args(),
+					'ret' => $ret,
+				) );
+				return $ret;
 			} );
 		}
 		return $this;
 	}
 
+	// todo: target should be filter
 	public function target( $func = null ) {
 		$this->target = $func;
 		return $this;
+	}
+
+	public function returned( $pos = false ) {
+		$data = $this->data;
+		if ( ! is_null( $this->target ) ) {
+			$data = $this->filter( $this->data, function( $arr ){
+				return $arr[ 'func' ] == $this->target;
+			} );
+		}
+		if ( is_numeric( $pos ) ) {
+			if (isset($data[ $pos ])) {
+				$data = $data[ $pos ][ 'ret' ];
+			} else {
+				$data = array();
+			}
+		} else {
+			$data = $this->map( $data, function( $arr ){
+				return $arr[ 'ret' ];
+			} );
+		}
+		$this->reset();
+		return $data;
 	}
 
 	public function called( $pos = false ) {
@@ -42,11 +77,11 @@ class Spy {
 			if ( is_null( $this->target ) ) {
 				$arr = reset( $this->intel );
 				if ( isset( $arr[ $pos ] ) ) {
-					$args = $arr[ $pos ];
+					$args = $arr[ $pos ][ 'arg' ];
 				}
 			} else {
 				if ( isset( $this->intel[ $this->target ][ $pos ] ) ) {
-					$args = $this->intel[ $this->target ][ $pos ];
+					$args = $this->intel[ $this->target ][ $pos ][ 'arg' ];
 				}
 			}
 			$this->reset();
@@ -55,13 +90,14 @@ class Spy {
 		return $this->count();
 	}
 
-	public function count() {
+	private function count() {
 		$count = 0;
 		if ( ! is_null( $this->target ) ) {
 			if ( isset( $this->intel[ $this->target ] ) ) {
 				$count = count( $this->intel[ $this->target ] );
 			}
 		} else {
+			// count all tracked
 			foreach( $this->intel as $arr ) {
 				$count += count( $arr );
 			}
@@ -72,6 +108,14 @@ class Spy {
 
 	private function reset() {
 		$this->target = null;
-		return $this;
+	}
+
+	private function filter( $arr, $cb ) {
+		$filtered = array_filter( $arr, $cb );
+		return array_values( $filtered );
+	}
+
+	private function map( $arr, $cb ) {
+		return array_map( $cb, $arr );
 	}
 }
--- test/test-spy.php
+++ test/test-spy.php
@@ -57,6 +57,43 @@ class SpyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( array( 'c', 'd' ), $spy->called(1) );
 		$this->assertEquals( array( 'g', 'h' ), $spy->target( 'user_func_b' )->called(1) );
 	}
+
+	function test_returned_with_single_target() {
+		$spy = new \DryRun\Spy( 'user_func_a' );
+
+		$this->assertEmpty( $spy->returned(), 'Should be empty' );
+		$this->assertEmpty( $spy->returned(0), 'Should be empty' );
+		$this->assertEmpty( $spy->target( 'user_func_a' )->returned(), 'Should be empty' );
+		$this->assertEmpty( $spy->target( 'user_func_a' )->returned(0), 'Should be empty' );
+
+		user_func_a( 'a', 'b' );
+		user_func_a( 'c', 'd' );
+
+		$this->assertEquals( array( 'b', 'd' ), $spy->returned(), 'Should have an array of all return values.' );
+		$this->assertEquals( 'b', $spy->returned(0), 'Should be the first return value.' );
+		$this->assertEquals( 'd', $spy->target( 'user_func_a' )->returned(1), 'Should be the second return value.' );
+	}
+
+	function test_returned_with_multiple_targets() {
+		$spy = new \DryRun\Spy( 'user_func_a', 'user_func_b' );
+
+		$this->assertEmpty( $spy->returned(), 'Should be empty' );
+		$this->assertEmpty( $spy->returned(0), 'Should be empty' );
+		$this->assertEmpty( $spy->target( 'user_func_b' )->returned(), 'Should be empty' );
+		$this->assertEmpty( $spy->target( 'user_func_b' )->returned(0), 'Should be empty' );
+
+		user_func_a( 'a', 'b' );
+		user_func_a( 'c', 'd' );
+
+		user_func_b( 'e', 'f' );
+		user_func_b( 'g', 'h' );
+
+		$this->assertEquals( array( 'b', 'd', 'f', 'h' ), $spy->returned(), 'Should have an array of return values form all targets.' );
+		$this->assertEquals( array( 'f', 'h' ), $spy->target( 'user_func_b' )->returned(), 'Should have an array of return values of target "user_func_b".' );
+		$this->assertEquals( 'f', $spy->returned(2), 'Should be the third return value from all targets.' );
+		$this->assertEquals( 'h', $spy->target( 'user_func_b' )->returned(1), 'Should be the second return value of target "user_func_b".' );
+		$this->assertEmpty( $spy->target( 'user_func_c' )->returned(), 'Should be empty' );
+	}
 }
 
 function user_func_a( $x, $y ) {
@@ -65,4 +102,8 @@ function user_func_a( $x, $y ) {
 
 function user_func_b( $x, $y ) {
 	return $y;
+}
+
+function user_func_c( $c ) {
+	return $c;
 }
